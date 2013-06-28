stewid/EpiContactTrace

commit ee8722c509febf397ba423799f563e15132e6997

Improved perfomance when calculating contact chain

src/trace.cpp
@@ -14,12 +14,6 @@ compareT(pair<int, int> const& t1,
   return t1.first < t2.first;
 }
 
-// Help method to count number of visited nodes
-bool IsVisited(const pair<bool, int>& node)
-{
-  return node.first;
-}
-
 // Lookup of ingoing and outgoing conatcts
 typedef pair<vector<map<int, Contacts> >, vector<map<int, Contacts> > > ContactsLookup;
 
@@ -187,43 +181,42 @@ SEXP traceContacts(SEXP src,
     vector<int> resultRowid;
     vector<int> resultDistance;
 
-    for(int i=0, end=rootVec.size(); i<end; ++i)
-      {
-	vector<pair<bool, int> > visitedNodesIngoing(as<int>(numberOfIdentifiers));
-	vector<pair<bool, int> > visitedNodesOutgoing(as<int>(numberOfIdentifiers));
+    for(int i=0, end=rootVec.size(); i<end; ++i) {
+      vector<pair<bool, int> > visitedNodesIngoing(as<int>(numberOfIdentifiers));
+      vector<pair<bool, int> > visitedNodesOutgoing(as<int>(numberOfIdentifiers));
 
-	resultRowid.clear();
-	resultDistance.clear();
+      resultRowid.clear();
+      resultDistance.clear();
 
-	traceContacts(lookup.first,
-		      rootVec[i] - 1,
-		      inBeginVec[i],
-		      inEndVec[i],
-		      visitedNodesIngoing,
-		      1,
-		      true,
-		      resultRowid,
-		      resultDistance);
+      traceContacts(lookup.first,
+		    rootVec[i] - 1,
+		    inBeginVec[i],
+		    inEndVec[i],
+		    visitedNodesIngoing,
+		    1,
+		    true,
+		    resultRowid,
+		    resultDistance);
 
-	result.push_back(resultRowid);
-	result.push_back(resultDistance);
+      result.push_back(resultRowid);
+      result.push_back(resultDistance);
 
-	resultRowid.clear();
-	resultDistance.clear();
+      resultRowid.clear();
+      resultDistance.clear();
 
-	traceContacts(lookup.second,
-		      rootVec[i] - 1,
-		      outBeginVec[i],
-		      outEndVec[i],
-		      visitedNodesOutgoing,
-		      1,
-		      false,
-		      resultRowid,
-		      resultDistance);
+      traceContacts(lookup.second,
+		    rootVec[i] - 1,
+		    outBeginVec[i],
+		    outEndVec[i],
+		    visitedNodesOutgoing,
+		    1,
+		    false,
+		    resultRowid,
+		    resultDistance);
 
-	result.push_back(resultRowid);
-	result.push_back(resultDistance);
-      }
+      result.push_back(resultRowid);
+      result.push_back(resultDistance);
+    }
 
     return result;
 }
@@ -241,26 +234,24 @@ degree(const vector<map<int, Contacts> >& data,
       ++it)
     {
       // We are not interested in going in loops.
-      if(node != it->first)
-	{
-	  // We are only interested in contacts within the specified time period,
-	  // so first check the lower bound, tBegin
-	  Contacts::const_iterator t_begin = lower_bound(it->second.begin(),
-							 it->second.end(),
-							 tBegin,
-							 CompareContact());
+      if(node != it->first) {
+	// We are only interested in contacts within the specified time period,
+	// so first check the lower bound, tBegin
+	Contacts::const_iterator t_begin = lower_bound(it->second.begin(),
+						       it->second.end(),
+						       tBegin,
+						       CompareContact());
 
-	  if(t_begin != it->second.end() && t_begin->t_ <= tEnd)
-	    {
-	      ++result;
-	    }
+	if(t_begin != it->second.end() && t_begin->t_ <= tEnd) {
+	  ++result;
 	}
+      }
     }
 
   return result;
 }
 
-void
+int
 contactChain(const vector<map<int, Contacts> >& data,
 	     int node,
 	     int tBegin,
@@ -268,6 +259,8 @@ contactChain(const vector<map<int, Contacts> >& data,
 	     vector<pair<bool, int> >& visitedNodes,
 	     bool ingoing)
 {
+  int numberOfVisitedNodes = 1;
+
   updateVisitedNodes(node, tBegin, tEnd, visitedNodes, ingoing);
 
   for(map<int, Contacts>::const_iterator it = data[node].begin(),
@@ -311,9 +304,11 @@ contactChain(const vector<map<int, Contacts> >& data,
 	  t1 = tEnd;
 	}
 
-	contactChain(data, it->first, t0, t1, visitedNodes, ingoing);
+	numberOfVisitedNodes += contactChain(data, it->first, t0, t1, visitedNodes, ingoing);
       }
     }
+
+  return numberOfVisitedNodes;
 }
 
 SEXP networkSummary(SEXP src,
@@ -345,42 +340,33 @@ SEXP networkSummary(SEXP src,
     vector<int> inDegree;
     vector<int> outDegree;
 
-    for(int i=0, end=rootVec.size(); i<end; ++i)
-      {
-	vector<pair<bool, int> > visitedNodesIngoing(as<int>(numberOfIdentifiers));
-	vector<pair<bool, int> > visitedNodesOutgoing(as<int>(numberOfIdentifiers));
+    for(int i=0, end=rootVec.size(); i<end; ++i) {
+      vector<pair<bool, int> > visitedNodesIngoing(as<int>(numberOfIdentifiers));
+      vector<pair<bool, int> > visitedNodesOutgoing(as<int>(numberOfIdentifiers));
 
-	contactChain(lookup.first,
-		     rootVec[i] - 1,
-		     inBeginVec[i],
-		     inEndVec[i],
-		     visitedNodesIngoing,
-		     true);
+      ingoingContactChain.push_back(contactChain(lookup.first,
+						 rootVec[i] - 1,
+						 inBeginVec[i],
+						 inEndVec[i],
+						 visitedNodesIngoing,
+						 true) - 1);
 
-	ingoingContactChain.push_back(count_if(visitedNodesIngoing.begin(),
-					       visitedNodesIngoing.end(),
-					       IsVisited) - 1);
+      outgoingContactChain.push_back(contactChain(lookup.second,
+						  rootVec[i] - 1,
+						  outBeginVec[i],
+						  outEndVec[i],
+						  visitedNodesOutgoing,
+						  false) - 1);
 
-	contactChain(lookup.second,
-		     rootVec[i] - 1,
-		     outBeginVec[i],
-		     outEndVec[i],
-		     visitedNodesOutgoing,
-		     false);
+      inDegree.push_back(degree(lookup.first,
+				rootVec[i] - 1,
+				inBeginVec[i],
+				inEndVec[i]));
 
-	outgoingContactChain.push_back(count_if(visitedNodesOutgoing.begin(),
-						visitedNodesOutgoing.end(),
-						IsVisited) - 1);
-
-	inDegree.push_back(degree(lookup.first,
-				  rootVec[i] - 1,
-				  inBeginVec[i],
-				  inEndVec[i]));
-
-	outDegree.push_back(degree(lookup.second,
-				   rootVec[i] - 1,
-				   outBeginVec[i],
-				   outEndVec[i]));
+      outDegree.push_back(degree(lookup.second,
+				 rootVec[i] - 1,
+				 outBeginVec[i],
+				 outEndVec[i]));
       }
 
     return List::create(_["inDegree"] = inDegree,

DESCRIPTION
@@ -8,8 +8,8 @@ Author: Stefan Widgren <stefan.widgren@sva.se>
     Maria Noremark <maria.noremark@sva.se>
 Description: Routines for epidemiological contact tracing
     and visualisation of network of contacts.
-Version: 0.7.5
-Date: 2013-01-29
+Version: 0.7.6
+Date: 2013-03-25
 Depends:
     R(>= 2.15.1),
     igraph0,

commit 9b650ff74f2407fd620f9a1802c4ee776e4b1682

Fix contact tracing
A bug was introduced when improving performance in commit ee8722c
The visited nodes were not used correct during contact tracing.
This is now fixed, using the previous code to assign VisitedNodes.
A test is added to check the results.

src/trace.cpp
@@ -119,20 +119,21 @@ traceContacts(const vector<map<int, Contacts> >& data,
 	      int node,
 	      int tBegin,
 	      int tEnd,
-	      VisitedNodes& visitedNodes,
+	      set<int> visitedNodes,
 	      int distance,
 	      bool ingoing,
 	      vector<int>& resultRowid,
 	      vector<int>& resultDistance)
 {
-  visitedNodes.Update(node, tBegin, tEnd, ingoing);
+  visitedNodes.insert(node);
 
   for(map<int, Contacts>::const_iterator it = data[node].begin(),
 	end = data[node].end();
       it != end;
       ++it)
     {
-      if(visitedNodes.Visit(it->first, tBegin, tEnd, ingoing)) {
+      // We are not interested in going in loops or backwards in the search path
+      if(visitedNodes.find(it->first) == visitedNodes.end()) {
 	// We are only interested in contacts within the specified time period,
 	// so first check the lower bound, tBegin
 	Contacts::const_iterator t_begin = lower_bound(it->second.begin(),
@@ -208,9 +209,6 @@ SEXP traceContacts(SEXP src,
     vector<int> resultDistance;
 
     for(int i=0, end=rootVec.size(); i<end; ++i) {
-      VisitedNodes visitedNodesIngoing(as<int>(numberOfIdentifiers));
-      VisitedNodes visitedNodesOutgoing(as<int>(numberOfIdentifiers));
-
       resultRowid.clear();
       resultDistance.clear();
 
@@ -218,7 +216,7 @@ SEXP traceContacts(SEXP src,
       		    rootVec[i] - 1,
       		    inBeginVec[i],
       		    inEndVec[i],
-      		    visitedNodesIngoing,
+      		    set<int>(),
       		    1,
       		    true,
       		    resultRowid,
@@ -234,7 +232,7 @@ SEXP traceContacts(SEXP src,
       		    rootVec[i] - 1,
       		    outBeginVec[i],
       		    outEndVec[i],
-      		    visitedNodesOutgoing,
+      		    set<int>(),
       		    1,
       		    false,
       		    resultRowid,

inst/tests/test-trace.R
@@ -552,3 +552,16 @@ test_that('Network summary', {
                 is_identical_to(ns))
 })
 
+test_that('Network summary using Trace', {
+    load(file="data/transfers.rda")
+
+    expect_that(NetworkSummary(Trace(transfers,
+                                     root=584,
+                                     tEnd='2005-10-31',
+                                     days=91)),
+                is_identical_to(NetworkSummary(transfers,
+                                               root=584,
+                                               tEnd='2005-10-31',
+                                               days=91)))
+})
+