e29aab53614cdfb32161276070a06e45f7c0cfad
Stefan Widgren <stefan.widgren@sva.se>
2014-02-03 16:38:10 +0100

    Added file tests/tree.R

---
+++ tests/tree.R
@@ -0,0 +1,45 @@
+## Copyright 2013-2014 Stefan Widgren and Maria Noremark,
+## National Veterinary Institute, Sweden
+##
+## Licensed under the EUPL, Version 1.1 or - as soon they
+## will be approved by the European Commission - subsequent
+## versions of the EUPL (the "Licence");
+## You may not use this work except in compliance with the
+## Licence.
+## You may obtain a copy of the Licence at:
+##
+## http://ec.europa.eu/idabc/eupl
+##
+## Unless required by applicable law or agreed to in
+## writing, software distributed under the Licence is
+## distributed on an "AS IS" basis,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+## express or implied.
+## See the Licence for the specific language governing
+## permissions and limitations under the Licence.
+
+library(EpiContactTrace)
+
+##
+## Node position checking
+##
+
+##
+## Case 1
+##
+tree <- structure(list(
+    node = c("O", "E", "F", "N", "A", "D", "G", "M", "B", "C", "H", "I", "J", "K", "L"),
+    parent = c(NA, "O", "O", "O", "E", "E", "N", "N", "D", "D", "M", "M", "M", "M", "M"),
+    level = c(0, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3),
+    x = c(NA_real_, NA_real_, NA_real_, NA_real_, NA_real_, NA_real_, NA_real_,
+        NA_real_, NA_real_, NA_real_, NA_real_, NA_real_, NA_real_,
+        NA_real_, NA_real_),
+    y = c(NA_real_, NA_real_, NA_real_, NA_real_, NA_real_, NA_real_, NA_real_,
+        NA_real_, NA_real_, NA_real_, NA_real_, NA_real_, NA_real_,
+        NA_real_, NA_real_),
+    prelim = c(NA_real_, NA_real_, NA_real_, NA_real_, NA_real_, NA_real_, NA_real_,
+        NA_real_, NA_real_, NA_real_, NA_real_, NA_real_, NA_real_, NA_real_, NA_real_),
+    modifier = c(NA_real_, NA_real_, NA_real_, NA_real_, NA_real_, NA_real_, NA_real_,
+    NA_real_, NA_real_, NA_real_, NA_real_, NA_real_, NA_real_, NA_real_, NA_real_)),
+                  .Names = c("node", "parent", "level", "x", "y", "prelim", "modifier"),
+                  row.names = c(NA, -15L), class = "data.frame")
