c914d6aed61fd60fee5e7d0e53b1299639d5501b
Stefan Widgren <stefan.widgren@sva.se>
2020-09-10 12:02:23 +0200

    Sync branches

--- R/EpiContactTrace-package.R
+++ R/EpiContactTrace-package.R
@@ -1,4 +1,4 @@
-## Copyright 2013 Stefan Widgren and Maria Noremark,
+## Copyright 2013-2020 Stefan Widgren and Maria Noremark,
 ## National Veterinary Institute, Sweden
 ##
 ## Licensed under the EUPL, Version 1.1 or - as soon they
@@ -20,36 +20,32 @@
 
 ##' Epidemiological tool for contact tracing.
 ##'
-##' Routines for epidemiological contact tracing and visualisation of network
-##' of contacts.
+##' Routines for epidemiological contact tracing and visualisation of
+##' network of contacts.
 ##'
 ##'
-##' In many countries, livestock movement data are
-##' collected with the major objective to enable contact tracing during disease
-##' outbreaks. Livestock movement data can also be of relevance for risk based
-##' surveillance - both during outbreak or when investigating if a disease is
-##' present in the population. However, the livestock movement databases are
-##' not always structured in such a way that relevant information for contact
-##' tracing or surveillance design is easily retrieved. EpiContactTrace uses
-##' the network parameters in-degree, out-degree, ingoing contact-chain and
-##' outgoing contact-chain, which are relevant for forward- and backward
-##' contact-tracing respectively. The measures can also be used for identifying
-##' herds with many contacts, which can be used in risk based disease
-##' surveillance. Different time periods for ingoing and outgoing contacts can
-##' be of interest in the contact tracing, based on possible window of
-##' introduction, and this can be adjusted in the tool. The output from the
-##' analysis is available as a dataset, but moreover, the tool automatically
-##' generates a report on farm level. The report both contains an overview of
-##' the situation on the farm, including a graph, as well as detailed
-##' information including dates of movements on group or individual level on
-##' all contacts.
+##' In many countries, livestock movement data are collected with the
+##' major objective to enable contact tracing during disease
+##' outbreaks. Livestock movement data can also be of relevance for
+##' risk based surveillance - both during outbreak or when
+##' investigating if a disease is present in the population. However,
+##' the livestock movement databases are not always structured in such
+##' a way that relevant information for contact tracing or
+##' surveillance design is easily retrieved. EpiContactTrace uses the
+##' network parameters in-degree, out-degree, ingoing contact-chain
+##' and outgoing contact-chain, which are relevant for forward- and
+##' backward contact-tracing respectively. The measures can also be
+##' used for identifying herds with many contacts, which can be used
+##' in risk based disease surveillance. Different time periods for
+##' ingoing and outgoing contacts can be of interest in the contact
+##' tracing, based on possible window of introduction, and this can be
+##' adjusted in the tool. The output from the analysis is available as
+##' a dataset, but moreover, the tool automatically generates a report
+##' on farm level. The report both contains an overview of the
+##' situation on the farm, including a graph, as well as detailed
+##' information including dates of movements on group or individual
+##' level on all contacts.
 ##'
-##' @name EpiContactTrace-package
-##' @aliases EpiContactTrace-package EpiContactTrace
-##' @docType package
-##' @author Stefan Widgren Maria Noremark
-##' @section Maintainer:
-##' Stefan Widgren <stefan.widgren@@sva.se>
 ##' @references \itemize{
 ##'   \item Dube, C., et al., A review of network analysis terminology
 ##'     and its application to foot-and-mouth disease modelling and policy
@@ -61,8 +57,9 @@
 ##'     disease control and riskbased surveillance.  Preventive Veterinary
 ##'     Medicine 99 (2011) 78-90, doi: 10.1016/j.prevetmed.2010.12.009
 ##' }
-##' @keywords package
 ##' @import methods
+##' @docType package
+##' @name EpiContactTrace-package
 ##' @useDynLib EpiContactTrace, .registration=TRUE
 ##' @examples
 ##'
@@ -70,36 +67,34 @@
 ##' data(transfers)
 ##'
 ##' ## Perform contact tracing
-##' contactTrace <- Trace(movements=transfers,
-##'                       root=2645,
-##'                       tEnd='2005-10-31',
-##'                       days=90)
+##' contactTrace <- Trace(movements = transfers,
+##'                       root = 2645,
+##'                       tEnd = "2005-10-31",
+##'                       days = 90)
 ##'
-##' show(contactTrace)
+##' contactTrace
 ##'
-##' \dontrun{
-##' ## Generate an html report showing details of the contact tracing for
-##' ## root 2645.
-##' ## Note: Creates the files 2645.html and 2645.png in the working
-##' ## directory.
-##' Report(contactTrace)
-##' }
-##'
+##' ## Generate an html report showing details of the contact tracing
+##' ## for root 2645. Note: Creates the files 2645.html and 2645.png
+##' ## in the temporary directory.
+##' Report(contactTrace, dir = tempdir())
 NULL
 
 ##' Movement Example Data
 ##'
-##' Movement data included in the package. The data contains fictitious
-##' example data of cattle movements during the period 2005-08-01 -- 2005-10-31.
+##' Movement data included in the package. The data contains
+##' fictitious example data of cattle movements during the period
+##' 2005-08-01 -- 2005-10-31.
 ##'
 ##'
 ##' @name transfers
 ##' @docType data
 ##' @usage data(transfers)
-##' @format A data frame with 70190 observations on the following 6 variables.
+##' @format A data frame with 70190 observations on the following 6
+##'     variables.
 ##' \describe{
 ##'   \item{source}{
-##'     a numeric vector with the holding identifier of the source
+##'     a numeric vector with the holding identifier of the source.
 ##'   }
 ##'
 ##'   \item{destination}{
@@ -129,8 +124,5 @@ NULL
 ##' @examples
 ##' data(transfers)
 ##'
-##' Trace(movements = transfers,
-##'       root = 2645,
-##'       tEnd = "2005-10-31",
-##'       days = 90)
+##' Trace(movements = transfers, root = 2645, tEnd = "2005-10-31", days = 90)
 NULL
