b30b4f4ece65dd386c3f3f4db8c22feadbea34dc
Stefan Widgren <stefan.widgren@gmail.com>
2020-10-04 14:28:32 +0200

    Indentation

--- R/show.R
+++ R/show.R
@@ -20,8 +20,9 @@
 
 ##' Show
 ##'
-##' Shows information of the time-window used for contact tracing and summary of
-##' network parameters. It also visualize the contact structure.
+##' Shows information of the time-window used for contact tracing and
+##' summary of network parameters. It also visualize the contact
+##' structure.
 ##'
 ##'
 ##' @name show-methods
@@ -47,14 +48,14 @@
 ##' }
 ##' @references \itemize{
 ##'   \item Dube, C., et al., A review of network analysis terminology
-##'     and its application to foot-and-mouth disease modelling and policy
-##'     development. Transbound Emerg Dis 56 (2009) 73-85, doi:
+##'     and its application to foot-and-mouth disease modelling and
+##'     policy development. Transbound Emerg Dis 56 (2009) 73-85, doi:
 ##'     10.1111/j.1865-1682.2008.01064.x
 ##'
-##'   \item Noremark, M., et al., Network analysis
-##'     of cattle and pig movements in Sweden: Measures relevant for
-##'     disease control and riskbased surveillance.  Preventive Veterinary
-##'     Medicine 99 (2011) 78-90, doi: 10.1016/j.prevetmed.2010.12.009
+##'   \item Noremark, M., et al., Network analysis of cattle and pig
+##'     movements in Sweden: Measures relevant for disease control and
+##'     riskbased surveillance.  Preventive Veterinary Medicine 99
+##'     (2011) 78-90, doi: 10.1016/j.prevetmed.2010.12.009
 ##' }
 ##' @examples
 ##' \dontrun{
@@ -70,65 +71,72 @@
 ##'
 ##' show(contactTrace)
 ##'}
-setMethod("show",
-          signature(object = "Contacts"),
-          function (object)
-      {
-          arrow <- ifelse(identical(object@direction, "in"), "<<<", ">>>")
-          prefix <- ifelse(identical(object@direction, "in"), "In", "Out")
+setMethod(
+    "show",
+    signature(object = "Contacts"),
+    function(object) {
+        arrow <- ifelse(identical(object@direction, "in"), "<<<", ">>>")
+        prefix <- ifelse(identical(object@direction, "in"), "In", "Out")
 
-          cat(sprintf("%s %s contacts %s\n", arrow, prefix, arrow))
+        cat(sprintf("%s %s contacts %s\n", arrow, prefix, arrow))
 
-          cat(sprintf("%s begin date: %s\n", prefix, object@tBegin))
-          cat(sprintf("%s end date:   %s\n", prefix, object@tEnd))
-          cat(sprintf("%s days: %i\n", prefix, object@tEnd - object@tBegin))
+        cat(sprintf("%s begin date: %s\n", prefix, object@tBegin))
+        cat(sprintf("%s end date:   %s\n", prefix, object@tEnd))
+        cat(sprintf("%s days: %i\n", prefix, object@tEnd - object@tBegin))
 
-          if(identical(object@direction, "out")) {
-              cat(sprintf("%s degree: %s\n", prefix, OutDegree(object)))
-              cat(sprintf("%sgoing contact chain: %i\n\n", prefix, OutgoingContactChain(object)))
-          } else {
-              cat(sprintf("%s degree: %s\n", prefix, InDegree(object)))
-              cat(sprintf("%sgoing contact chain: %i\n\n", prefix, IngoingContactChain(object)))
-          }
+        if (identical(object@direction, "out")) {
+            cat(sprintf("%s degree: %s\n", prefix, OutDegree(object)))
+            cat(sprintf("%sgoing contact chain: %i\n\n",
+                        prefix, OutgoingContactChain(object)))
+        } else {
+            cat(sprintf("%s degree: %s\n", prefix, InDegree(object)))
+            cat(sprintf("%sgoing contact chain: %i\n\n",
+                        prefix, IngoingContactChain(object)))
+        }
 
-          if(length(object@source) > 0L) {
-              arrow <- ifelse(identical(object@direction, "out"), "-->", "<--")
-              width <- max(nchar(object@source), nchar(object@destination))
-              format <- sprintf("%%s%% %is %s %% %is\n", width, arrow, width)
+        if (length(object@source) > 0L) {
+            arrow <- ifelse(identical(object@direction, "out"),
+                            "-->", "<--")
+            width <- max(nchar(object@source), nchar(object@destination))
+            format <- sprintf("%%s%% %is %s %% %is\n",
+                              width, arrow, width)
 
-              ## Get network structure. The distance is used for indentation.
-              ns <- NetworkStructure(object)
+            ## Get network structure. The distance is used for
+            ## indentation.
+            ns <- NetworkStructure(object)
 
-              ## Rename source and destination to lhs and rhs, with respect to direction
-              if(identical(object@direction, "out")) {
-                  names(ns)[names(ns) == "source"] <- "lhs"
-                  names(ns)[names(ns) == "destination"] <- "rhs"
-              } else {
-                  names(ns)[names(ns) == "source"] <- "rhs"
-                  names(ns)[names(ns) == "destination"] <- "lhs"
-              }
+            ## Rename source and destination to lhs and rhs, with
+            ## respect to direction
+            if (identical(object@direction, "out")) {
+                names(ns)[names(ns) == "source"] <- "lhs"
+                names(ns)[names(ns) == "destination"] <- "rhs"
+            } else {
+                names(ns)[names(ns) == "source"] <- "rhs"
+                names(ns)[names(ns) == "destination"] <- "lhs"
+            }
 
-              for(i in seq_len(nrow(ns))) {
-                  cat(sprintf(format,
-                              paste(rep(" ", (ns$distance[i] - 1) * (width + 5)), collapse=""),
-                              ns$lhs[i],
-                              ns$rhs[i]))
-              }
+            for (i in seq_len(nrow(ns))) {
+                line <- rep(" ", (ns$distance[i] - 1) * (width + 5))
+                cat(sprintf(format,
+                            paste(line, collapse = ""),
+                            ns$lhs[i],
+                            ns$rhs[i]))
+            }
+        } else {
+            cat(sprintf("No %sgoing contacts during the search period.\n",
+                        object@direction))
+        }
 
-          } else {
-              cat(sprintf("No %sgoing contacts during the search period.\n", object@direction))
-          }
-
-          cat("\n")
-      }
+        cat("\n")
+    }
 )
 
-setMethod("show",
-          signature(object = "ContactTrace"),
-          function (object)
-      {
-          cat(sprintf("Root: %s\n\n", object@root))
-          show(object@ingoingContacts)
-          show(object@outgoingContacts)
-      }
+setMethod(
+    "show",
+    signature(object = "ContactTrace"),
+    function(object) {
+        cat(sprintf("Root: %s\n\n", object@root))
+        show(object@ingoingContacts)
+        show(object@outgoingContacts)
+    }
 )
